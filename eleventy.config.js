8b04c329791651466143c3be4402c782186c370b
Paul <paul@theapplegates.com>
2024-09-08 21:24:40 -0400

    done

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,14 +2,26 @@
  *  @param {import("@11ty/eleventy/src/UserConfig")} eleventyConfig
  */
 
-export default async function (eleventyConfig) {
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+
+export default function(eleventyConfig) {
   eleventyConfig.addWatchTarget('./src/*');
   eleventyConfig.addPassthroughCopy('src/*.{css,js,jpg,ico}');
+  eleventyConfig.addPassthroughCopy("assets/images");
+  
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+      extensions: "html",
+      formats: ["avif", "webp", "jpeg"],
+      defaultAttributes: {
+        loading: "lazy",
+        decoding: "async",
+      },
+    });
 
   return {
     dir: {
       output: 'dist',
       input: 'src'
-    }
+    },
   };
 }
